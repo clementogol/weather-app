815f2775338d0270516c87f344cfebcabc2ad354
clogol <clementogol@gmail.com>
2025-06-25 11:00:40 +0300

    doc: upadted screenshot

--- src/Weather.jsx
+++ src/Weather.jsx
@@ -5,9 +5,12 @@ const API_KEY = import.meta.env.VITE_OPENWEATHER_API_KEY;
 
 function Weather() {
   const [weatherData, setWeatherData] = useState(null)
+  const [loading, setLoading] = useState(false)
   const [city, setCity] = useState('nairobi')
 
   const fetchWeatherData = async(cityName) => {
+
+    setLoading(true)
     const url = `${BASE_URL}?q=${cityName}&appid=${API_KEY}&units=metric`
 
     const response = await fetch(url)
@@ -15,6 +18,7 @@ function Weather() {
     const data = await response.json()
 
     setWeatherData(data)
+    setLoading(false)
   }
 
   useEffect(() => {
@@ -64,6 +68,15 @@ function Weather() {
           </div>
         </form>
 
+        {loading && (
+          <div className="flex flex-col items-center justify-center py-10">
+            {/* Spinner */}
+            <div className="animate-spin rounded-full h-12 w-12 border-4 border-blue-600 border-t-transparent mb-4"></div>
+            {/* Loader Text */}
+            <p className="text-blue-800 font-semibold">Fetching weather data...</p>
+          </div>
+        )}
+
         {
           weatherData && (
             <div className='bg-white rounded-lg shadow-lg p-6'>
@@ -71,7 +84,39 @@ function Weather() {
                 <h2 className='text-2xl font-bold text-gray-800'>
                   {weatherData.name}, {weatherData.sys.country}
                 </h2>
+                <p className='text-gray-600 capitalize'>{weatherData.weather[0].description}</p>
               </div>
+              
+              <div>
+                <img
+                src={`https://openweathermap.org/img/wn/${weatherData.weather[0].icon}@2x.png`} alt={weatherData.weather[0].description}
+                className='mx-auto mb-2'
+                />
+
+                <div
+                className='text-4xl font-bold text-gray-800 text-center mb-2'>{Math.round(weatherData.main.temp)}&deg;C
+                </div>
+                
+                <div className="grid grid-cols-2 gap-4 mt-6 text-center">
+                  <div>
+                    <div className="text-sm text-gray-500">Humidity</div>
+                    <div className="text-lg font-semibold text-blue-800">{weatherData.main.humidity}%</div>
+                  </div>
+                  <div>
+                    <div className="text-sm text-gray-500">Clouds</div>
+                    <div className="text-lg font-semibold text-blue-800">{weatherData.clouds.all}%</div>
+                  </div>
+                  <div>
+                    <div className="text-sm text-gray-500">Wind</div>
+                    <div className="text-lg font-semibold text-blue-800">{weatherData.wind.speed} m/s</div>
+                  </div>
+                  <div>
+                    <div className="text-sm text-gray-500">Pressure</div>
+                    <div className="text-lg font-semibold text-blue-800">{weatherData.main.pressure} hPa</div>
+                  </div>
+                </div>
+
+              </div> 
             </div>
           )
         }
